769ae19fa334abf86511f176dddd36c22dfac00b
Vincent Bengtsson <vinben9928@edu.halmstad.se>
2018-10-18 10:07:28 +0200

    Added an array and looped through its data.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-    echo "Hello World!";
+    $data = [123, 456, "Hej", false, true, 123.2];
+
+    foreach($data as $key => $val) {
+        echo "[" . $key . "] " . $val . "<br/>";
+    }
 ?>

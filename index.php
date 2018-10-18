4f0c8c0bae4f3cc572dd3ebd4712fa627e48e335
Vincent Bengtsson <vinben9928@edu.halmstad.se>
2018-10-18 10:11:17 +0200

    Added items outside array initializer.

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-    $data = [123, 456, "Hej", false, true, 123.2];
+    $data = [];
+
+    //Three ways to add items.
+    //1
+    array_push($data, "Hello");
+    array_push($data, "World");
+
+    //2
+    $data[] = "Next item";
+
+    //3
+    $data[1234] = "This is magic!";
 
     foreach($data as $key => $val) {
         echo "[" . $key . "] " . $val . "<br/>";
